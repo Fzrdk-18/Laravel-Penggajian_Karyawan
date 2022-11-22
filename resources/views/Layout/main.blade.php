6cee30d5f561ac55966ba03ec55b20e3ff7d1d44
Fzrdk-18 <fazliradika06@gmail.com>
2022-11-22 09:44:15 +0700

    new file:   .editorconfig 	new file:   .env.example 	new file:   .gitattributes 	new file:   .gitignore 	new file:   README.md 	new file:   app/Console/Kernel.php 	new file:   app/Exceptions/Handler.php 	new file:   app/Http/Controllers/Controller.php 	new file:   app/Http/Controllers/IndexController.php 	new file:   app/Http/Kernel.php 	new file:   app/Http/Middleware/Authenticate.php 	new file:   app/Http/Middleware/EncryptCookies.php 	new file:   app/Http/Middleware/PreventRequestsDuringMaintenance.php 	new file:   app/Http/Middleware/RedirectIfAuthenticated.php 	new file:   app/Http/Middleware/TrimStrings.php 	new file:   app/Http/Middleware/TrustHosts.php 	new file:   app/Http/Middleware/TrustProxies.php 	new file:   app/Http/Middleware/ValidateSignature.php 	new file:   app/Http/Middleware/VerifyCsrfToken.php 	new file:   app/Models/User.php 	new file:   app/Providers/AppServiceProvider.php 	new file:   app/Providers/AuthServiceProvider.php 	new file:   app/Providers/BroadcastServiceProvider.php 	new file:   app/Providers/EventServiceProvider.php 	new file:   app/Providers/RouteServiceProvider.php 	new file:   artisan 	new file:   bootstrap/app.php 	new file:   bootstrap/cache/.gitignore 	new file:   composer.json 	new file:   composer.lock 	new file:   config/app.php 	new file:   config/auth.php 	new file:   config/broadcasting.php 	new file:   config/cache.php 	new file:   config/cors.php 	new file:   config/database.php 	new file:   config/filesystems.php 	new file:   config/hashing.php 	new file:   config/logging.php 	new file:   config/mail.php 	new file:   config/queue.php 	new file:   config/sanctum.php 	new file:   config/services.php 	new file:   config/session.php 	new file:   config/view.php 	new file:   database/.gitignore 	new file:   database/factories/UserFactory.php 	new file:   database/migrations/2014_10_12_000000_create_users_table.php 	new file:   database/migrations/2014_10_12_100000_create_password_resets_table.php 	new file:   database/migrations/2019_08_19_000000_create_failed_jobs_table.php 	new file:   database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php 	new file:   database/migrations/2022_11_01_070712_create_jabatan_table.php 	new file:   database/migrations/2022_11_01_071239_create_karyawan_table.php 	new file:   database/migrations/2022_11_01_071327_create_gaji_table.php 	new file:   database/seeders/DatabaseSeeder.php 	new file:   lang/en/auth.php 	new file:   lang/en/pagination.php 	new file:   lang/en/passwords.php 	new file:   lang/en/validation.php 	new file:   package.json 	new file:   phpunit.xml 	new file:   public/.htaccess 	new file:   public/css/bootstrap.min.css 	new file:   public/favicon.ico 	new file:   public/images/logo/logo.png 	new file:   public/index.php 	new file:   public/js/bootstrap.min.js 	new file:   public/robots.txt 	new file:   resources/css/app.css 	new file:   resources/js/app.js 	new file:   resources/js/bootstrap.js 	new file:   resources/views/Layout/main.blade.php 	new file:   resources/views/dashboard.blade.php 	new file:   routes/api.php 	new file:   routes/channels.php 	new file:   routes/console.php 	new file:   routes/web.php 	new file:   storage/app/.gitignore 	new file:   storage/app/public/.gitignore 	new file:   storage/framework/.gitignore 	new file:   storage/framework/cache/.gitignore 	new file:   storage/framework/cache/data/.gitignore 	new file:   storage/framework/sessions/.gitignore 	new file:   storage/framework/testing/.gitignore 	new file:   storage/framework/views/.gitignore 	new file:   storage/logs/.gitignore 	new file:   tests/CreatesApplication.php 	new file:   tests/Feature/ExampleTest.php 	new file:   tests/TestCase.php 	new file:   tests/Unit/ExampleTest.php 	new file:   vite.config.js

---
+++ resources/views/Layout/main.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset("css/bootstrap.min.css") }}">
+</head>
+<body class="bg-warning-70">
+    {{----------------------NAVBAR------------------}}
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container"
+            <a class="navbar-brand text-light" href="/Home"> 
+                <img src="{{ asset("images/logo/logo.png") }}" alt="Logo"
+                width="40" height="40" class="me-2">
+                <strong>PT. BARYON NH</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-
+            toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav"
+            aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active text-light fs-5" aria-current="page" href="/Home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light fs-5" href="/jabatan/index">jabatan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light fs-5" href="/Gaji">Gaji</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light fs-5" href="/Laporan">Laporan</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+        {{----------------------CONTENT------------------}}
+        <div class="div container">
+            <div class="row m-4">
+                <div class="col">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+        {{----------------------JAVASCRIPT--------------}}
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    
+</body>
+</html>
